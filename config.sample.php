5e43c107c547e4f36fefc7fc3c5701c93f22a7a1
Frank <goodman.capu@gmail.com>
2018-11-17 17:10:28 -0800

    Create config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Configuration for Simple Site.
+ *
+ * You need to copy this file to `config.php` to make it work.
+ *
+ * This file contains the following configrations:
+ *
+ * - MySQL settings.
+ *
+ * Reference:
+ * - https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
+ */
+ 
+//** MySQL settings. **//
+
+/** The database username. */
+define('SIMPLE_SITE_DB_USERNAME', 'database_username_here');
+
+/** The database password. */
+define('SIMPLE_SITE_DB_PASSWORD', 'database_password_here');
+
+/** The database name. */
+define('SIMPLE_SITE_DB_NAME', 'database_name_here');
+
+/** The database hostname. */
+define('SIMPLE_SITE_DB_HOSTNAME', 'localhost');
+
+//** Debug settings. **//
+
+/** Whether server should report errors. */
+define('SIMPLE_SITE_REPORT_ERRORS', false);
