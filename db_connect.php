913f39c15e224068a979d4e174a697f5a898ed37
Frank <goodman.capu@gmail.com>
2018-11-17 17:12:03 -0800

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Global shared lib for Simple Site.
+ */
+require_once 'config.php';
+
+/**
+ * Database connector.
+ */
+function db_connect() {
+    $con = mysqli_connect(SIMPLE_SITE_DB_HOSTNAME, SIMPLE_SITE_DB_USERNAME, SIMPLE_SITE_DB_PASSWORD, SIMPLE_SITE_DB_NAME) or die("Database connection failed!");
+    $con->query("SET NAMES 'UTF8MB4'");
+    return $con;
+}
