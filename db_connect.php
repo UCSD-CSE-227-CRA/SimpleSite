b381a24042798e9f81e110de38ff85684ab8dc56
Frank <goodman.capu@gmail.com>
2018-11-17 17:14:02 -0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,8 @@ require_once 'config.php';
  * Database connector.
  */
 function db_connect() {
-    $con = mysqli_connect(SIMPLE_SITE_DB_HOSTNAME, SIMPLE_SITE_DB_USERNAME, SIMPLE_SITE_DB_PASSWORD, SIMPLE_SITE_DB_NAME) or die("Database connection failed!");
+    $con = mysqli_connect(SIMPLE_SITE_DB_HOSTNAME, SIMPLE_SITE_DB_USERNAME, SIMPLE_SITE_DB_PASSWORD, SIMPLE_SITE_DB_NAME)
+        or die("Database connection failed, check your 'config.php'!");
     $con->query("SET NAMES 'UTF8MB4'");
     return $con;
 }
