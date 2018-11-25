db2fffa074363a9447a40816b8a7c85e425a286a
Frank <goodman.capu@gmail.com>
2018-11-25 15:51:42 -0800

    Refractor

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 /**
  * Update the next token according to raw token and secret
  */
-function update_token(cookie) {
+function update_token() {
     let cookie_prefix = localStorage.getItem("simple_site_cookie_prefix");
     let secret = localStorage.getItem("simple_site_secret");
-    const secret_matches = new RegExp("([0-9a-zA-Z_]+)secret=([0-9a-zA-Z]+)").exec(cookie);
+    const secret_matches = new RegExp("([0-9a-zA-Z_]+)secret=([0-9a-zA-Z]+)").exec(document.cookie);
     if (secret_matches !== null) {
         cookie_prefix = secret_matches[1];
         secret = secret_matches[2];
@@ -16,13 +16,13 @@ function update_token(cookie) {
     }
 
     if (secret !== null) {
-        const raw_token_matches = new RegExp(cookie_prefix + "raw_token=([0-9a-zA-Z]+)").exec(cookie);
+        const raw_token_matches = new RegExp(cookie_prefix + "raw_token=([0-9a-zA-Z]+)").exec(document.cookie);
         if (raw_token_matches !== null) {
             const raw_token = raw_token_matches[1];
             delete_cookie(cookie_prefix + "raw_token");
             set_cookie(cookie_prefix + "token", md5(secret + raw_token));
         } else {
-            const token_matches = new RegExp(cookie_prefix + "token=([0-9a-zA-Z]+)").exec(cookie);
+            const token_matches = new RegExp(cookie_prefix + "token=([0-9a-zA-Z]+)").exec(document.cookie);
             if (token_matches === null) {
                 console.error("No token found");
             }
