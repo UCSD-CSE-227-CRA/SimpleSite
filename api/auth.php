8006c9f7606ca5bb6a388f38cf674e42e6eef753
Frank <goodman.capu@gmail.com>
2018-12-01 16:59:06 -0800

    Added comments

--- api/auth.php
+++ api/auth.php
@@ -10,18 +10,20 @@ require_once 'utilities.php';
 function check_login($con) {
     $sid = filter($con, $_POST["sid"]);
     $token = filter($con, $_POST["token"]);
-    $info = filter($con, $_POST["info"]);
-    $info_encrypted = filter($con, $_POST["info_encrypted"]);
+    $info = filter($con, $_POST["info"]); // Correspond to "url" field in front end
+    $info_encrypted = filter($con, $_POST["info_encrypted"]); // Correspond to "url_encrypted" field in front end
     if (strlen($sid) == 0 || !is_random_string($sid, 32) || !is_random_string($token, 32)
         || strlen($info) == 0 || !is_random_string($info_encrypted, 32) || $info_encrypted === $token) {
         return -1;
     }
 
+    // Check whether the session ID and token is matched
     $result = $con->query("SELECT * FROM session WHERE sid = '$sid' AND token = '$token'");
     check_sql_error($con);
     if (mysqli_affected_rows($con) > 0) {
         $result = mysqli_fetch_array($result);
         $secret = $result['secret'];
+        // Check whether the info and encrypted info is matched
         if (md5($secret . $token . $info) != $info_encrypted) {
             return -1;
         }
